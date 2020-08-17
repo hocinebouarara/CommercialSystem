4db87b6c7f20f349dea97c610ae2ff78482a037c
hocinebouarara <hocinbouarara08@gmail.com>
2020-08-18 00:23:04 +0100

    create method of get layout of add suppliers

--- src/suppliers/SuppliersViewController.java
+++ src/suppliers/SuppliersViewController.java
@@ -9,6 +9,8 @@ import clients.ClientsViewController;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import helpers.DbConnect;
+import helpers.Links;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,8 +22,11 @@ import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -29,6 +34,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Callback;
 import models.Client;
 import models.Supplier;
@@ -165,6 +172,18 @@ public class SuppliersViewController implements Initializable {
 
     @FXML
     private void addMembers(MouseEvent event) {
+         try {
+            Parent root = FXMLLoader.load(getClass().getResource("/suppliers/addSupplier.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource(Links.VIEWSTYLE).toExternalForm());
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException ex) {
+            Logger.getLogger(ClientsViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
