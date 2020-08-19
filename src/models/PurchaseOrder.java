ecadfcbc547708c26338d4d8fc03a7fa1a7803fd
hocinebouarara <hocinbouarara08@gmail.com>
2020-08-19 15:49:07 +0100

    create purchase order model

--- src/models/PurchaseOrder.java
+++ src/models/PurchaseOrder.java
@@ -5,10 +5,111 @@
  */
 package models;
 
+import java.sql.Date;
+
 /**
  *
  * @author hocin
  */
 public class PurchaseOrder {
+    
+    int orderId,productId;
+
+    String reference, designation;
+    
+    int quantity,supplierId;
+    
+    String SupplierName;
+    
+    Date orderDate,deliveryDate;
+
+    public PurchaseOrder(int orderId, int productId, String reference, String designation, int quantity, int supplierId, String SupplierName, Date orderDate, Date deliveryDate) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.reference = reference;
+        this.designation = designation;
+        this.quantity = quantity;
+        this.supplierId = supplierId;
+        this.SupplierName = SupplierName;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSupplierName() {
+        return SupplierName;
+    }
+
+    public void setSupplierName(String SupplierName) {
+        this.SupplierName = SupplierName;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+    
+    
+    
+    
+    
     
 }
