38a0824ad837bb832f9b5a29ffc7c2fc39301224
hocinebouarara <hocinebouarara@hocine95>
2020-10-15 20:32:10 +0100

    add method get view of purchases

--- src/home/HomeViewController.java
+++ src/home/HomeViewController.java
@@ -104,7 +104,7 @@ public class HomeViewController implements Initializable {
 
     @FXML
     private void getPurchasesView(MouseEvent event) {
-        loadViews(Links.PURCHASESORDERS);
+        loadViews(Links.PURCHASES);
     }
 
     @FXML
