20c136690d283c00a0efbc4ab7709842afa21419
hocinebouarara <hocinebouarara@hocine95>
2020-10-15 20:31:07 +0100

    we design view of purchase

--- src/purchases/PurchasesController.java
+++ src/purchases/PurchasesController.java
@@ -5,16 +5,40 @@
  */
 package purchases;
 
+import com.jfoenix.controls.JFXButton;
+import helpers.Links;
+import home.HomeViewController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
  *
- * @author hocinebouarara
+ * @author hocin
  */
 public class PurchasesController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Pane ordersPane;
+    @FXML
+    private Pane deliveriesPane;
+    @FXML
+    private JFXButton pyme;
+    @FXML
+    private Pane payPane;
 
     /**
      * Initializes the controller class.
@@ -22,6 +46,46 @@ public class PurchasesController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        deliveriesPane.setVisible(false);
+        ordersPane.setVisible(false);
+        payPane.setVisible(false);
+
+    }
+
+    @FXML
+    public void getOrdersView(MouseEvent event) {
+        loadViews(Links.PURCHASESORDERS);
+        deliveriesPane.setVisible(false);
+        payPane.setVisible(false);
+        ordersPane.setVisible(true);
+
+    }
+
+    @FXML
+    private void getDeliveriesView(MouseEvent event) {
+        loadViews(Links.PURCHASESDELIVERIES);
+        ordersPane.setVisible(false);
+        payPane.setVisible(false);
+        deliveriesPane.setVisible(true);
+    }
+
+    private void loadViews(String viewName) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource(viewName));
+            anchorPane.getChildren().setAll(pane);
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @FXML
+    private void getPayementView(MouseEvent event) {
+        loadViews(Links.PURCHASESPAY);
+        ordersPane.setVisible(false);
+        deliveriesPane.setVisible(false);
+        payPane.setVisible(true);
+    }
+
+
 }
