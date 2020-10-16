d1a200324becd9191b366825a75a87ed2d58ba80
hocinebouarara <hocinebouarara@hocine95>
2020-10-16 01:52:40 +0100

    create method search user if exist ?

--- src/login/LoginViewController.java
+++ src/login/LoginViewController.java
@@ -5,9 +5,24 @@
  */
 package login;
 
+import com.mysql.jdbc.PreparedStatement;
+import helpers.DbConnect;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -16,6 +31,18 @@ import javafx.fxml.Initializable;
  */
 public class LoginViewController implements Initializable {
 
+    @FXML
+    private TextField userNameFld;
+    @FXML
+    private PasswordField passwordFld;
+    
+   
+    String query = null;
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    private boolean checkUser = false;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +50,38 @@ public class LoginViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    
+     public boolean checkUsername(String username) {
+
+        query = "SELECT * FROM `employee` WHERE `USEM` =?";
+
+        try {
+
+            connection = DbConnect.getConnect();
+            preparedStatement = (PreparedStatement) connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                checkUser = true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return checkUser;
+    }
+
+    @FXML
+    private void onKeyTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void signIn(MouseEvent event) {
+    }
+
+    @FXML
+    private void close(MouseEvent event) {
+    }
     
 }
