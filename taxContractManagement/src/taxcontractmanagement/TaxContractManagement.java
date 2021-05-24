cd18ff64bf5dfd618f3c5591fa2505ba7fde0dcd
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-24 15:17:28 +0100

    modify the first view running

--- taxContractManagement/src/taxcontractmanagement/TaxContractManagement.java
+++ taxContractManagement/src/taxcontractmanagement/TaxContractManagement.java
@@ -22,7 +22,7 @@ public class TaxContractManagement extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(Links.LOGINVIEW));
+        Parent root = FXMLLoader.load(getClass().getResource(Links.HOMEVIEW));
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
