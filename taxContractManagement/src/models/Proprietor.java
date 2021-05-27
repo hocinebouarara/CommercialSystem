55966aa1e8c3318a26b828676fb16b5a64e6658e
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-27 18:02:38 +0100

    create the model of proprietor

---
+++ taxContractManagement/src/models/Proprietor.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Deeplight
+ */
+public class Proprietor {
+    
+    int id ;
+    String name ;
+    Date date ;
+    String adresse ,phone;
+
+    public Proprietor(int id, String name, Date date, String adresse, String phone) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.adresse = adresse;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+
+    
+    
+}
