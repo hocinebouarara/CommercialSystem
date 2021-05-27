fa78584dc138905b69ef1db5738d5d75b74bc73b
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-27 18:04:01 +0100

    change the design of proprietor view with its controller

--- taxContractManagement/src/proprietors/ProprietorsViewController.java
+++ taxContractManagement/src/proprietors/ProprietorsViewController.java
@@ -5,9 +5,19 @@
  */
 package proprietors;
 
+import helpres.Links;
+import home.HomeViewController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -16,12 +26,53 @@ import javafx.fxml.Initializable;
  */
 public class ProprietorsViewController implements Initializable {
 
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Pane editPane;
+    @FXML
+    private Pane editPane1;
+    @FXML
+    private AnchorPane proprAnchor;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        loadViews(Links.PROPRIETORTABLEVIEW);
+       
+    }
+
+  
     
+    private void loadViews(String viewName) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource(viewName));
+            proprAnchor.getChildren().setAll(pane);
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+
+
+    @FXML
+    private void getAddProprView(MouseEvent event) {
+        loadViews(Links.ADDPROPIETORVIEW);
+    }
+
+    @FXML
+    private void proprietaireView() {
+        
+        loadViews(Links.PROPRIETORTABLEVIEW);
+        
+    }
+
+    @FXML
+    private void statView(MouseEvent event) {
+    }
+
 }
