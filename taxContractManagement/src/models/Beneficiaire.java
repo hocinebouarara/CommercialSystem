f646ddb847c5a837daf6c0153cad0c1ff3688a82
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-28 19:18:47 +0100

    create the beneficiary module

---
+++ taxContractManagement/src/models/Beneficiaire.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Deeplight
+ */
+public class Beneficiaire {
+
+    int id;
+    String name;
+    Date date;
+    String wilaya, commune, nationnalite;
+
+    public Beneficiaire(int id, String name, Date date, String wilaya, String commune, String nationnalite) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.wilaya = wilaya;
+        this.commune = commune;
+        this.nationnalite = nationnalite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getWilaya() {
+        return wilaya;
+    }
+
+    public void setWilaya(String wilaya) {
+        this.wilaya = wilaya;
+    }
+
+    public String getCommune() {
+        return commune;
+    }
+
+    public void setCommune(String commune) {
+        this.commune = commune;
+    }
+
+    public String getNationnalite() {
+        return nationnalite;
+    }
+
+    public void setNationnalite(String nationnalite) {
+        this.nationnalite = nationnalite;
+    }
+    
+    
+
+}
