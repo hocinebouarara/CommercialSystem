6a72124aba0905b9496f1e53bbdb60fd7c653a7f
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-28 18:44:45 +0100

    create the controller of add view

--- taxContractManagement/src/beneficiaries/AddBeneficiaireController.java
+++ taxContractManagement/src/beneficiaries/AddBeneficiaireController.java
@@ -5,10 +5,20 @@
  */
 package beneficiaries;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import models.Beneficiaire;
+import models.Proprietor;
 
 /**
  * FXML Controller class
@@ -17,6 +27,24 @@ import javafx.fxml.Initializable;
  */
 public class AddBeneficiaireController implements Initializable {
 
+    @FXML
+    private TextField nameField;
+    @FXML
+    private JFXDatePicker dateFld;
+    @FXML
+    private TextField communeFld;
+    @FXML
+    private TextField wilayaField;
+    @FXML
+    private TextField nationaFld;
+
+    String query = null;
+    Connection connection = null;
+    ResultSet resultSet = null;
+    PreparedStatement preparedStatement;
+    Beneficiaire beneficiaire = null;
+    private boolean update;
+    int beneficiaireId;
     /**
      * Initializes the controller class.
      */
@@ -26,11 +54,17 @@ public class AddBeneficiaireController implements Initializable {
     }    
 
     void setUpdate(boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     void setTextField(int id, String name, LocalDate toLocalDate, String wilaya, String commune, String nationnalite) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @FXML
+    private void save(MouseEvent event) {
+    }
+
+    @FXML
+    private void clean(MouseEvent event) {
     }
     
 }
