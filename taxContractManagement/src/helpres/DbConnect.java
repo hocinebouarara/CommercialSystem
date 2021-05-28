7c365421af9d7a9c92824354be563c5fc3012870
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-28 19:14:18 +0100

    updating the data base

--- taxContractManagement/src/helpres/DbConnect.java
+++ taxContractManagement/src/helpres/DbConnect.java
@@ -17,7 +17,7 @@ public class DbConnect {
 
     private static final String HOST = "127.0.0.1";
     private static final int PORT = 3306;
-    private static final String DB_NAME = "taxcontract";
+    private static final String DB_NAME = "bd";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     private static Connection connection;
