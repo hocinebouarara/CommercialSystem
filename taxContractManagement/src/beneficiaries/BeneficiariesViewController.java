a0f20a873728a43f81099696103dcaeb6903a84a
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-28 19:06:28 +0100

    create the beneficiary controller and methods of get children views

--- taxContractManagement/src/beneficiaries/BeneficiariesViewController.java
+++ taxContractManagement/src/beneficiaries/BeneficiariesViewController.java
@@ -5,9 +5,19 @@
  */
 package beneficiaries;
 
+import helpres.Links;
+import home.HomeViewController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -16,12 +26,46 @@ import javafx.fxml.Initializable;
  */
 public class BeneficiariesViewController implements Initializable {
 
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Pane editPane;
+    @FXML
+    private Pane editPane1;
+    @FXML
+    private AnchorPane beneficAnchor;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        getBeneficTable();
     }    
+    
+     private void loadViews(String viewName) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource(viewName));
+            beneficAnchor.getChildren().setAll(pane);
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @FXML
+    private void getBeneficTable() {
+        loadViews(Links.BENEFICIAIRETABLEVIEW);
+    }
+
+    @FXML
+    private void statView(MouseEvent event) {
+    }
+
+    @FXML
+    private void getAddBeneficView(MouseEvent event) {
+        loadViews(Links.ADDBENEFICIAIREVIEW);
+    }
     
 }
