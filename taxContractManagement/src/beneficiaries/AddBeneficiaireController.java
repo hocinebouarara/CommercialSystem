8b02ce05b0ea201a6a06e9d112d182fd01e18b49
Deeplight <Deeplight@DESKTOP-AJH32EO>
2021-05-28 18:00:28 +0100

    initialize add beneficiary view and its controller

---
+++ taxContractManagement/src/beneficiaries/AddBeneficiaireController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beneficiaries;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Deeplight
+ */
+public class AddBeneficiaireController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    void setUpdate(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setTextField(int id, String name, LocalDate toLocalDate, String wilaya, String commune, String nationnalite) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
